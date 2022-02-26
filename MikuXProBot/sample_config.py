d9bdd42357dfe98c02f66f5f89a3a76e41adcdbc
Miss Sylveon <94614122+MissSylveon@users.noreply.github.com>
2022-02-26 19:36:55 +0530

    Update sample_config.py

--- MikuXProBot/sample_config.py
+++ MikuXProBot/sample_config.py
@@ -14,14 +14,14 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "awoo"  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "awoo"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 769830161 # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "awoo"
+    API_ID = "5bcc548f108e30a913a339a4f97f315c"  # integer value, dont use ""
+    API_HASH = "4759912"
+    TOKEN = "5080111815:AAGL8mY1Fb6rwfDzQVLpraBGGsvBlpkUxE4"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1323719429 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Aryanjawale"
     SUPPORT_CHAT = 'mikussupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -123456789  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -123446789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    JOIN_LOGGER = -1001282990849  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001282990849  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
@@ -30,7 +30,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "M7r11AlyDZGvFh2F9U6umCS96iVvyMC~Bo9khw2cKMcYvRBYyDOUNJ7FLQqEEwgP"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     #OPTIONAL
@@ -51,8 +51,8 @@ class Config(object):
     WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
     BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
-    TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
+    CASH_API_KEY = '24IWG7JGXMR46QEE'  # Get your API key from https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = 'YGTK7XPATUHK'  # Get your API key from https://timezonedb.com/api
     WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
     AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
